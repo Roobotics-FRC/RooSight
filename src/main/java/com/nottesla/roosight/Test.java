5a99e7dd99243f4e7f2a45f218b34d38f8445fbd
aaplmath <jrrotella@icloud.com>
2017-02-14 16:26:54 -0500

    Remove unnecessary lib loads

--- src/main/java/com/nottesla/roosight/Test.java
+++ src/main/java/com/nottesla/roosight/Test.java
@@ -12,26 +12,13 @@ import java.io.File;
 public class Test {
     public static void main(String[] args) throws InterruptedException {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        System.loadLibrary("opencv_imgproc.3.2.0");
-        System.loadLibrary("opencv_imgcodecs.3.2");
-        System.loadLibrary("opencv_videoio.3.2");
-        System.loadLibrary("opencv_highgui.3.2.0");
-        System.loadLibrary("opencv_flann.3.2");
-        System.loadLibrary("opencv_ml.3.2");
-        System.loadLibrary("opencv_features2d.3.2");
-        System.loadLibrary("opencv_calib3d.3.2");
-        System.loadLibrary("opencv_objdetect.3.2");
-        System.loadLibrary("opencv_photo.3.2");
-        System.loadLibrary("opencv_stitching.3.2");
-        System.loadLibrary("opencv_video.3.2.0");
-        System.loadLibrary("opencv_superres.3.2");
-        System.loadLibrary("opencv_shape.3.2");
-        System.loadLibrary("opencv_videostab.3.2");
-        Mat mat = new Mat((long) 13837483);
+
+        Mat mat = new Mat((long) 18364734);
+//        Mat mat1 = new Mat();
 //        process(new File("/tmp/images/0.jpg"));
         File files[] = new File("/tmp/images").listFiles();
         for (int i = 0; i < files.length; ++i) {
-            process(files[i]);
+//            process(files[i]);
         }
     }
 
