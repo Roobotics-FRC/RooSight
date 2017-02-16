5ae7f026c9e76c4cb89725c2dcffc493d68cb1d0
aaplmath <jrrotella@icloud.com>
2017-02-16 18:10:12 -0500

    Add CLT functionality to work with RooSightGUI

--- src/main/java/org/usfirst/frc/team4373/roosight/Test.java
+++ src/main/java/org/usfirst/frc/team4373/roosight/Test.java
@@ -5,6 +5,7 @@ import org.opencv.core.Mat;
 
 import java.awt.*;
 import java.io.File;
+import java.util.*;
 
 /**
  * Created by tesla on 4/15/16.
@@ -13,13 +14,68 @@ public class Test {
     public static void main(String[] args) throws InterruptedException {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-        Mat mat = new Mat((long) 18364734);
-//        Mat mat1 = new Mat();
-//        process(new File("/tmp/images/0.jpg"));
-        File files[] = new File("/tmp/images").listFiles();
-        for (int i = 0; i < files.length; ++i) {
-            process(files[i]);
+        java.util.List list = Arrays.asList(args);
+        File file = null;
+
+        if (list.indexOf("-file") > -1) {
+            file = new File((String) list.get(list.indexOf("-file") + 1));
         }
+
+        if (file != null) {
+            int index = -1;
+            if (list.indexOf("-hsv") > -1) {
+                index = list.indexOf("-hsv");
+                processHSV(file, (String) list.get(index + 1));
+            } else if (list.indexOf("-hsl") > -1) {
+                index = list.indexOf("-hsl");
+                processHSL(file, (String) list.get(index + 1));
+            } else if (list.indexOf("-rgb") > -1) {
+                index = list.indexOf("-rgb");
+                processRGB(file, (String) list.get(index + 1));
+            }
+        }
+    }
+
+    public static void processHSV(File file, String params) {
+        RooColorImage colorImage = new RooColorImage(file.getAbsolutePath());
+        RooConfig config = new RooConfig();
+        String[] paramArray = params.split(",");
+        if (paramArray.length == 6) {
+            config.setHSV(new Integer(paramArray[0]), new Integer(paramArray[1]), new Integer(paramArray[2]), new Integer(paramArray[3]), new Integer(paramArray[4]), new Integer(paramArray[5]));
+            writeConfigured(colorImage, config, file);
+        }
+    }
+
+    public static void processHSL(File file, String params) {
+        RooColorImage colorImage = new RooColorImage(file.getAbsolutePath());
+        RooConfig config = new RooConfig();
+        String[] paramArray = params.split(",");
+        if (paramArray.length == 6) {
+            config.setHSL(new Integer(paramArray[0]), new Integer(paramArray[1]), new Integer(paramArray[2]), new Integer(paramArray[3]), new Integer(paramArray[4]), new Integer(paramArray[5]));
+            writeConfigured(colorImage, config, file);
+        }
+    }
+
+    public static void processRGB(File file, String params) {
+        RooColorImage colorImage = new RooColorImage(file.getAbsolutePath());
+        RooConfig config = new RooConfig();
+        String[] paramArray = params.split(",");
+        if (paramArray.length == 6) {
+            config.setRGB(new Integer(paramArray[0]), new Integer(paramArray[1]), new Integer(paramArray[2]), new Integer(paramArray[3]), new Integer(paramArray[4]), new Integer(paramArray[5]));
+            writeConfigured(colorImage, config, file);
+        }
+    }
+
+    public static void writeConfigured(RooColorImage colorImage, RooConfig config, File file) {
+        config.setMinArea(1000);
+        config.setMinPerimeter(300);
+        RooProcessor rooProcessor = new RooProcessor(config);
+        RooBinaryImage thresh = rooProcessor.processImage(colorImage);
+        thresh.blur(1);
+        RooContour[] contours = rooProcessor.findContours(thresh);
+        colorImage.drawContours(contours, new RooColor(Color.RED), 2);
+        colorImage.markContours(contours, new RooColor(Color.RED), 1);
+        colorImage.writeToFile(file.getAbsolutePath() + ".out.jpg");
     }
 
     public static void process(File file) {
